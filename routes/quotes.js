44cc1f4da25f6e86f15d74659e9508b51ff1f6f6
Geshan Manandhar <geshan.manandhar@theiconic.com.au>
2021-10-29 20:49:53 +1100

    Adds the POST Quote API
    - [x] Also adds a new quote to the db

--- routes/quotes.js
+++ routes/quotes.js
@@ -12,4 +12,14 @@ router.get('/', function(req, res, next) {
   }
 });
 
+/* POST quote */
+router.post('/', function(req, res, next) {
+  try {
+    res.json(quotes.create(req.body));
+  } catch(err) {
+    console.error(`Error while adding quotes `, err.message);
+    next(err);
+  }
+});
+
 module.exports = router;
