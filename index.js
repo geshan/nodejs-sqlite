01c30063b3070936608d7db2caa4481c0560f35d
Giulio <giuliohome@gmail.com>
2022-06-30 12:05:00 +0000

    fix issue #5 port from env var

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000 || process.env.PORT;
+const port = process.env.PORT || 3000;
 const quotesRouter = require('./routes/quotes');
 
 app.use(express.json());
